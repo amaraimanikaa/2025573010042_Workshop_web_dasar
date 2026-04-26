bd963197128b7f1a7adbc6101904f9ef7ef036e4
amaraimanikaa <amaraimanika83@gmail.com>
2026-04-26 16:30:41 +0700

    bagian-4-komponen UI & dark mode

--- modul-5/src/main.js
+++ modul-5/src/main.js
@@ -1,1 +1,15 @@
-import './style.css'
+import "./style.css";
+
+const btn = document.getElementById("toggleDark");
+const html = document.documentElement;
+// Cek preferensi tersimpan
+if (localStorage.getItem("theme") === "dark") {
+  html.classList.add("dark");
+  btn.textContent = "light";
+}
+btn.addEventListener("click", () => {
+  html.classList.toggle("dark");
+  const isDark = html.classList.contains("dark");
+  btn.textContent = isDark ? "light" : "dark";
+  localStorage.setItem("theme", isDark ? "dark" : "light");
+});
